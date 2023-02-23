140d93501414cfca11f482a437d55aede7837871
Tom Turner <tom@twt.me.uk>
2023-02-23 11:56:33 +0000

    Work done on testing

---
+++ django_advanced_pdf/tests.py
@@ -0,0 +1,88 @@
+import pathlib
+import unittest
+from pathlib import Path
+import fitz
+from django_advanced_pdf.engine.report_xml import ReportXML
+from PIL import Image, ImageChops
+
+
+class PDFTests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    @staticmethod
+    def get_test_folder():
+        return Path(Path(__file__).resolve().parent, 'test_data')
+
+    def run_report(self, name):
+        test_folder = self.get_test_folder()
+        temp_folder = Path(test_folder, 'temp', name)
+
+        pathlib.Path(temp_folder).mkdir(parents=True, exist_ok=True)
+
+        png_files = list(temp_folder.glob("*.png"))
+
+        # Delete each PNG file
+        for png_file in png_files:
+            png_file.unlink()
+
+        with open(Path(test_folder, 'reports', f'{name}.xml')) as f:
+            xml = f.read()
+
+        report_xml = ReportXML()
+        result = report_xml.load_xml_and_make_pdf(xml=xml)
+        matrix = fitz.Matrix(300 / 72, 300 / 72)
+
+        with fitz.open("pdf", result) as doc:
+            for page_number, page in enumerate(doc, 1):
+                output_file = Path(temp_folder, f'{page_number}.png')
+
+                pix = page.get_pixmap(matrix=matrix)
+                pix.pil_save(output_file, format="PNG", dpi=(300, 300))
+
+            page_count = len(doc)
+
+            with open(Path(temp_folder, 'page_count.txt'), 'w') as w:
+                w.write(str(page_count))
+
+        self.check_report(name=name, new_page_count=page_count)
+
+    def check_report(self, name, new_page_count):
+        test_folder = self.get_test_folder()
+        temp_folder = Path(test_folder, 'temp', name)
+        held_folder = Path(test_folder, 'held', name)
+
+        with open(Path(held_folder, 'page_count.txt'), 'r') as f:
+            self.assertEqual(int(f.read()), new_page_count, msg='Pages count not equal')
+
+        for page_number in range(1, new_page_count+1):
+
+            # Load the two images
+            image1 = Image.open(Path(held_folder, f'{page_number}.png'))
+            image2 = Image.open(Path(temp_folder, f'{page_number}.png'))
+
+            # Check if the images are the same
+            if ImageChops.difference(image1, image2).getbbox() is not None:
+
+                # Create a new image showing the difference
+                diff_image = ImageChops.difference(image1, image2)
+                # Save the difference image
+
+                error_folder = Path(test_folder, 'errors')
+                pathlib.Path(error_folder).mkdir(parents=True, exist_ok=True)
+
+                diff_image.save(Path(error_folder, f'{name}.png'))
+                self.assertTrue(False, f'Page {page_number} is different')
+
+    def test_keep_with_next(self):
+        self.run_report(name='keep_with_next')
+
+    def test_basic(self):
+        self.run_report(name='basic')
+
+    def test_change_header(self):
+        self.run_report(name='change_header')
+
+    def test_border(self):
+        self.run_report(name='border')
