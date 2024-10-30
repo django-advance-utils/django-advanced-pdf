4ab8ee14ea2f07a6f126abf8190b84139f39cc5c
Tom Turner <tom@twt.me.uk>
2024-10-30 22:49:21 +0000

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-advanced-pdf",
-    version="0.2.14",
+    version="0.2.15",
     author="Thomas Turner",
     description="Django app that helps one create pdf",
     long_description=long_description,
